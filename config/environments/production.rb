d1b0083943fa4e1569bd4c71eb742b86410458fc
Zachary Allaun <zach.allaun@gmail.com>
2015-07-30 15:33:09 -0400

    Use file store again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,10 +35,6 @@ Rails.application.configure do
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
 
-  config.assets.configure do |env|
-    env.cache = ActiveSupport::Cache::MemCacheStore.new
-  end
-
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Specifies the header that your server uses for sending files.
